7c9265e55a07d51e5ded3f98be40a3c1b762a545
Frobi <frobi.webdev@outlook.com>
2022-07-26 18:04:19 +0930

    create-classes-for-board-and-players

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,27 @@
+class Board
+    @@board = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9']
+    ]
+
+    def self.board # displays the board as a grid when printed
+        @@board.each do |r|
+            puts r.each { |p| p }.join(" ")
+        end
+    end
+end
+
+class Player < Board
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+frobe = Player.new("Frobe")
+
+p Player.board
 
