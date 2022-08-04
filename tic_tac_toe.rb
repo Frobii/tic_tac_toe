d2d5bbabf3876e63ded36071ab52454d7b79e460
Frobi <frobi.webdev@outlook.com>
2022-08-04 18:47:47 +0930

    implement-stricter-rules-on-user-input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -27,49 +27,6 @@ class Board
         ]
     end
 
-    def winner_found(player)
-        puts "#{player} won!"
-        puts "Would you like to play again?"
-        playAgain = gets.chomp.upcase
-        if playAgain == "Y"
-            reset_board()
-        else
-            @@game_status = "over"
-        end
-    end
-
-    def draw()
-        p board()
-        puts "It's a draw!"
-        puts "Would you like to play again?"
-        playAgain = gets.chomp.upcase
-        if playAgain == "Y"
-            reset_board()
-        else
-            @@game_status = "over"
-        end
-    end
-
-    def winner_check(player, playerSymbol)
-        if @@board.any? do |row| row.all? { |symbol| symbol == playerSymbol } end
-            winner_found(player) # check all three horizontal wins
-        elsif @@board[0][0] == playerSymbol && @@board[1][0] == playerSymbol && @@board[2][0] == playerSymbol
-            winner_found(player) # check the first vertical win
-        elsif @@board[0][1] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][1] == playerSymbol
-            winner_found(player) # check the second vertical win
-        elsif @@board[0][2] == playerSymbol && @@board[1][2] == playerSymbol && @@board[2][2] == playerSymbol
-            winner_found(player) # check the third vertical win
-        elsif @@board[0][0] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][2] == playerSymbol
-            winner_found(player) # check "backslash" win
-        elsif @@board[0][2] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][0] == playerSymbol
-            winner_found(player) # check "forward slash" win
-        elsif @@board.flatten.sort.join("").include? "OOOOO"
-            draw() # check for a draw where p1 picked "O"
-        elsif @@board.flatten.sort.join("").include? "XXXXX"
-            draw() # check for a draw where p1 picked "X"
-        end
-    end
-
 end
 
 class Player < Board
@@ -77,10 +34,8 @@ class Player < Board
     attr_reader :p1symbol
     attr_reader :p2symbol
 
-
     @@player_count = 0
     @@choice_symbol = ""
-
 
     def initialize(name)
         @name = name
@@ -94,8 +49,9 @@ class Player < Board
    def symbol_pick()
         if @@player_count == 1
             puts "Pick your symbol: O / X"
-            @answer = gets.chomp
+            @answer = gets.chomp.upcase
             if @answer != "X" && @answer != "O"
+                puts "\n"
                 puts "Please enter X or O"
                 symbol_pick()
             else 
@@ -124,15 +80,60 @@ class Player < Board
         winner_check(player, playerSymbol)
     end
 
+    def play_again()
+        playAgain = gets.chomp.upcase
+        if playAgain == "Y"
+            reset_board()
+        elsif playAgain == "N"
+            @@game_status = "over"
+        else
+            puts "\n"
+            puts "Please make a valid selection: y/n"
+            play_again()
+        end
+    end
+
+    def winner_found(player)
+        puts "\n"
+        puts "#{player} won!"
+        p Player.board()
+        puts "Would you like to play again? y/n"
+        play_again()
+    end
+
+    def draw()
+        puts "\n"
+        puts "It's a draw!"
+        p Player.board()
+        puts "Would you like to play again? y/n"
+        play_again()
+    end
+
+    def winner_check(player, playerSymbol)
+        if @@board.any? do |row| row.all? { |symbol| symbol == playerSymbol } end
+            winner_found(player) # check all three horizontal wins
+        elsif @@board[0][0] == playerSymbol && @@board[1][0] == playerSymbol && @@board[2][0] == playerSymbol
+            winner_found(player) # check the first vertical win
+        elsif @@board[0][1] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][1] == playerSymbol
+            winner_found(player) # check the second vertical win
+        elsif @@board[0][2] == playerSymbol && @@board[1][2] == playerSymbol && @@board[2][2] == playerSymbol
+            winner_found(player) # check the third vertical win
+        elsif @@board[0][0] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][2] == playerSymbol
+            winner_found(player) # check "backslash" win
+        elsif @@board[0][2] == playerSymbol && @@board[1][1] == playerSymbol && @@board[2][0] == playerSymbol
+            winner_found(player) # check "forward slash" win
+        elsif @@board.flatten.sort.join("").include? "OOOOO"
+            draw() # check for a draw where p1 picked "O"
+        elsif @@board.flatten.sort.join("").include? "XXXXX"
+            draw() # check for a draw where p1 picked "X"
+        end
+    end
 end
-
-
 
 puts "Player 1, enter your name:"
 player1 = Player.new(gets.chomp)
 
 player1.symbol_pick()
-
 
 puts "Player 2, enter your name:"
 player2 = Player.new(gets.chomp)
@@ -142,6 +143,7 @@ player2.symbol_pick()
 puts "\n"
 
 i = 1
+
 until Board.game_status == "over"
     if i % 2 == 0 # use modulo to swap between turns 
         player2.play_round(player2.name, player2.p2symbol)
