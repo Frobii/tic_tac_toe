184c4889813589014bb89a49059de2366ceab4ab
frobee <frobi.webdev@outlook.com>
2023-01-16 12:14:44 +1030

    create-tests-for-wins-and-losses

--- spec/ttt-spec.rb
+++ spec/ttt-spec.rb
@@ -1,8 +1,36 @@
 require_relative "../lib/tic_tac_toe.rb"
 
-describe Player do
-    
-    context "when three of the same symbols line up" do
-        
+describe TicTacToe do
+    let(:game) { TicTacToe.new }
+  
+    context "when X wins" do
+      before do
+        game.board[0][0] = :X
+        game.board[0][1] = :X
+        game.board[0][2] = :X
+      end
+  
+      it "returns X as the winner" do
+        expect(game.winner).to eq :X
+      end
     end
+  
+    context "when O wins" do
+      before do
+        game.board[0][0] = :O
+        game.board[0][1] = :O
+        game.board[0][2] = :O
+      end
+  
+      it "returns O as the winner" do
+        expect(game.winner).to eq :O
+      end
+    end
+  
+    context "when there is no winner" do
+      it "returns nil" do
+        expect(game.winner).to be_nil
+      end
+    end
+
 end
